efec41b3858557b51222683a0b7ca38c75b18bdc
Sachin-Shanmugam <sachinshanmugam19@gmail.com>
2025-08-28 14:32:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-//your JS code here. If required.
 function daysOfAYear(year) {
-	if ((year % 4 === 0 && year % 100 !== 0) || year % 400 ===0){
-		return 366;
-	}else{
-		return 365;
-	}
-	}
-
-console.log(daysOfAYear(2022)); // 365
-console.log(daysOfAYear(2024)); // 366
-console.log(daysOfAYear(1900)); // 365
-console.log(daysOfAYear(2000)); // 366
-
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return 366;
+    } else {
+        return 365;
+    }
 }
+
+let input = prompt("Enter a year:");
+input = Number(input);
+alert(daysOfAYear(input));
